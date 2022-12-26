54c1debaf1736395b9bcc2004197ed68b49ea73e
rihabz <rihabziani154@gmail.com>
2022-12-26 10:58:40 +0100

    commit

---
+++ Customer-Service1/src/main/java/com/example/customerservice1/CustomerService1Application.java
@@ -0,0 +1,13 @@
+package com.example.customerservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerService1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerService1Application.class, args);
+    }
+
+}
